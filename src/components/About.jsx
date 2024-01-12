5afc04a1557a790d2f86a73327c1b92463970196
Prasadlohakare25 <prasadlohakare25@gmail.com>
2024-01-12 23:38:35 +0530

    Updated the version for mobile

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -7,11 +7,11 @@ function About() {
                 <div className='pb-8'>
                     <p className='text-4xl font-bold inline border-b-4 border-gray-500'>About</p>
                 </div>
-                <p className='text-xl mt-20'>
+                <p className=' text-lg md:text-xl mt-20'>
                     Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tenetur, quae ipsum? Dolorum sequi, ratione ut laudantium illo nihil exercitationem in asperiores libero atque impedit ea doloremque, itaque nulla fugit corrupti, quisquam totam recusandae saepe quos iure vel iste hic. Incidunt voluptatum error voluptas laboriosam? Mollitia debitis fugiat voluptates autem atque.
                 </p>
                 <br />
-                <p className='text-xl'>
+                <p className='text-lg md:text-xl'>
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora nulla odit debitis eaque dicta maiores atque provident? Fugit totam laudantium vero facere laboriosam labore dolorem enim exercitationem voluptas consectetur fuga placeat quibusdam ea, ipsa quae ullam. Dolore, rem fuga odio ut consequatur incidunt? Tempore iste velit officiis numquam cumque quibusdam?
                 </p>
             </div>
--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -51,7 +51,7 @@ function Experience() {
         <div name="Experience" className='w-full h-full bg-gradient-to-b from-gray-800 to-black'>
             <div className='max-w-screen-lg mx-auto p-4 flex flex-col justify-center w-full h-full text-white'>
                 <div>
-                    <p className='text-4xl font-bold border-b-4 border-gray-500 p-2 inline'>
+                    <p className='text-3xl md:text-4xl font-bold border-b-4 border-gray-500 p-2 inline'>
                         Experience
                     </p>
                     <p className='py-6'>
